3256e20a766fb772f63c137b4967d4a54bbc21e9
Miroslav <tomic.miroslav97@gmail.com>
2020-02-14 15:11:35 +0100

    Adding cors mappings

--- src/main/java/clinic/centersystem/config/WebConfig.java
+++ src/main/java/clinic/centersystem/config/WebConfig.java
@@ -12,5 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins("https://centersystemfrontend.herokuapp.com/");
     }
 }
