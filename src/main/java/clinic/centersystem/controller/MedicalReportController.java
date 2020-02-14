620c1179abdd8738a0fb5010ecec50da07e4b223
Miroslav <tomic.miroslav97@gmail.com>
2020-02-14 18:09:06 +0100

    Edit mapping

--- src/main/java/clinic/centersystem/controller/MedicalReportController.java
+++ src/main/java/clinic/centersystem/controller/MedicalReportController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/med-rep", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MedicalReportController {
 
